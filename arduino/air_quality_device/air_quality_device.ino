537db1c0b87d80da3cc324756ab30801489a15a7
Andrew <andrewgall12@gmail.com>
2022-11-07 18:51:23 -0500

    Fix arduino stuff

--- arduino/air_quality_device/air_quality_device.ino
+++ arduino/air_quality_device/air_quality_device.ino
@@ -1,10 +1,48 @@
+#include <WiFiNINA.h>
+#include "secrets.h"
+
+#define URL "auto-air-quality-system.herokuapp.com" // Change to real url
+
+WiFiClient client;
+
 void setup() {
-  // put your setup code here, to run once:
   Serial.begin(9600);
+  while (!Serial) {
+  }
+  Serial.println("Connecting to WiFi...");
+  WiFi.begin(WIFI_ID, WIFI_PASS);
+  Serial.println("Connected!");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("Test");
-  delay(1000);
+  int value = 100;
+  String model = "Test2";
+  String data = "{\"params\": { \"data\": { \"model\": \"" + model + "\", \"value\": \"" + String(value) + "\"}}}";
+  sendData(data);
+
+  // Log response
+  while (client.available()) {
+    char c = client.read();
+    Serial.write(c);
+  }
+  delay(5000);
+}
+
+void sendData(String body) {
+  if (WiFi.status() != WL_CONNECTED) {
+    return;
+  }
+  if (client.connect(URL, 80)) {
+    Serial.println("Sending data to server...");
+    client.println("POST /api/user/air-quality-data HTTP/1.0");
+    client.println("Host: " + String(URL));
+    client.println("Content-Type: application/json");
+    client.println("Accept: */*");
+    client.println("Accept-Encoding: gzip, deflate, br");
+    client.println("Content-Length: " + String(body.length()));
+    client.println("Connection: keep-alive");
+    client.println();
+    client.println(body);
+    Serial.println("Sent!");
+  }
 }
