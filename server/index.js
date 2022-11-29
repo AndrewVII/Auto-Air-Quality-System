aa361ee92fdc9de8ce9376502e228b8a31c2daa5
Andrew <andrewgall12@gmail.com>
2022-11-29 00:11:24 -0500

    fix

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ const io = new Server(httpServer);
 connectDB();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 app.enable('trust proxy', true);
 app.set('trust proxy', 1);
 app.use(express.static('dist'));
@@ -38,8 +38,6 @@ io.use(ios(expressSession));
 app.use('/static', express.static('dist'));
 app.use('*/js', express.static('dist'));
 app.use('*/txt', express.static('dist'));
-
-app.use(express.bodyParser({ limit: '50mb' }));
 
 // attach user to req object
 app.all('*', async (req, res, next) => {
